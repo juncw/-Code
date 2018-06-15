c5e32ab3bad59365e26c3c6e400ffaaea6a93603
Junchao-PC\Junchao <kindy_wjc@163.com>
2018-06-15 23:51:37 +0800

    添加注释

--- 汉诺塔/main.cpp
+++ 汉诺塔/main.cpp
@@ -2,11 +2,14 @@
 // 问题描述：将多个盘从A盘移动到C盘，借助B盘。
 // 借助递归问题进行求解，也就是暴力递归问题
 // 考虑问题有无“后效性”的问题
+// 一个汉诺塔的问题N层就是T(N) = T(N-1)+1+T(N-1)
 #include <iostream>
 using namespace std;
 
 
 void hanNuoTowel(int floor, char A, char help, char C) {
+	// 注意在中断条件，如果不满足就执行非中断问题点。
+	// 防止在中断问题后自动调用非中断函数
 	if (floor == 1) {
 		cout << "from->" << A << "->move to->" << C << endl;
 	}
